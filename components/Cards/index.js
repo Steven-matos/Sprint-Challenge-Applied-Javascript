beeb164bf7ecd40144b50bdaedfc08099545f877
Steven <38477361+Steven-matos@users.noreply.github.com>
2020-01-17 12:15:26 -0500

    cards for each topic created

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,42 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+.then(res => {
+    console.log(res.data.articles)
+    Object.entries(res.data.articles).forEach(article => {
+        console.log(article[1])
+        article[1].forEach(info =>
+            document.querySelector('.cards-container').append(Cards(info)))
+    })
+})
+.catch(err => {
+    console.error(err); 
+})
+
+function Cards(data) {
+    const card = document.createElement('div'),
+          headline = document.createElement('div'),
+          authorContainer = document.createElement('div'),
+          imgContainer = document.createElement('div'),
+          cardImg = document.createElement('img'),
+          author = document.createElement('span');
+
+    card.append(headline);
+    card.append(authorContainer);
+    authorContainer.append(imgContainer);
+    authorContainer.append(author);
+    imgContainer.append(cardImg);
+
+    card.classList.add('card');
+    headline.classList.add('headline');
+    authorContainer.classList.add('author');
+    imgContainer.classList.add('img-container');
+
+    headline.textContent = data.headline;
+    cardImg.src = data.authorPhoto;
+    author.textContent = `By ${data.authorName}`;
+
+    return card;
+}
